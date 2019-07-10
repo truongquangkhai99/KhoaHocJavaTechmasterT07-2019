55786f0180ece881f89674a1d03e56af332e6e63
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-07-10 21:31:13 +0700

    first lession

---
+++ bai01/src/com/myapp/Calculation.java
@@ -0,0 +1,28 @@
+package com.myapp;
+
+public class Calculation {
+    //function = method
+    public static Integer sum2Numbers(Integer x, Integer y) {
+        //x, y = input parameters
+        return  x + y;
+    }
+    public static Double minus2Numbers(Double x, Double y) {
+        return x - y;
+    }
+    public static void sayHello(String message) {
+        System.out.println("I say: "+message);
+    }
+    public static void doSomething() {
+        Integer[] integers = {1, 5, 4, 6, 6, 7, 8, 9};//this is an array
+        //iterate an array
+        Integer numberOfEven = 0;
+        for(int i = 0; i < integers.length; i++) {
+            //System.out.println("number ="+integers[i]);
+            if(integers[i] % 2 == 0) {
+                System.out.println("number ="+integers[i]);
+                numberOfEven++;
+            }
+        }
+        System.out.println("Number of evens = "+numberOfEven);
+    }
+}
---
+++ bai01/src/com/myapp/Main.java
@@ -0,0 +1,13 @@
+package com.myapp;
+
+public class Main {
+    public static  void main(String [] args) {
+        //args = arguments
+        Calculation.sayHello("Chao cac ban");
+        Integer sum = Calculation.sum2Numbers(3, 2);
+        System.out.println("Tong = "+sum);
+        System.out.println(" 10 -5 ="+Calculation.minus2Numbers(10.0, 5.0));
+        Calculation.doSomething();
+    }
+
+}
