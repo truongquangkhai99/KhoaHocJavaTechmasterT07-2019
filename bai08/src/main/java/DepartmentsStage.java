3cf052e94b27004795b8a0fb6873b06cfde829d9
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-08-02 21:30:10 +0700

    update

---
+++ bai08/src/main/java/DepartmentsStage.java
@@ -0,0 +1,8 @@
+import javafx.stage.Stage;
+
+public class DepartmentsStage extends Stage {
+    private LoginStage loginStage;
+    DepartmentsStage(LoginStage loginStage) {
+        this.loginStage = loginStage;
+    }
+}
